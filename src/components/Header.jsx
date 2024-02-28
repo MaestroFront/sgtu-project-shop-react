cde17e846db906927924be59b63e0a8dd524c71b
Maestro <akievaleksandr@mail.ru>
2024-02-28 21:54:58 +0500

    Add react and clear projects
    - Add react settings
    - Clear projects for a good starting
    - Add Header, Main, Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div>
+                <span className="logo">Shop Name</span>
+            </div>
+            <div className="presentation"></div>
+        </header>
+    );
+}
